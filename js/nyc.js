4d4784609738cf3d68049d06a9f758aef373831f
eddiep7 <146613466+eddiep7@users.noreply.github.com>
2025-03-12 11:04:32 -0700

    Update nyc.js

--- js/nyc.js
+++ js/nyc.js
@@ -23,7 +23,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZWR3YXJkcDciLCJhIjoiY2xzZjZhZnYwMGdrbDJpcXB6M
         // END OF NAVIGATION
         const map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/dark-v11',
+            style: 'mapbox://styles/edwardp7/cm867wiwc008c01rg8xesbkl8',
             center: [-74.006, 40.7128],
             zoom: 10
         });
